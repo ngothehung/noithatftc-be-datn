641b3ead316971b7d6b9f6223db12549e44920bb
ngothehung <ngothehung2110@gmail.com>
2023-11-19 16:37:36 +0700

    update-api

--- api/src/frontend/user/dto/UpdateEmailUser.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
-
-class UpdateEmailUserDto {
-    @IsString()
-    @IsNotEmpty()
-    email: string;
-}
-
-export default UpdateEmailUserDto;
--- api/src/frontend/user/dto/UpdatePhoneUser.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
-
-class UpdatePhoneUserDto {
-    @IsString()
-    @IsNotEmpty()
-    phone: string;
-}
-
-export default UpdatePhoneUserDto;
